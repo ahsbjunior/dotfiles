a129ad13902d31c71b535c63dea654f21d9ecaf4
Afonso Junior <afonso@mobits.com.br>
2024-10-17 18:48:40 -0300

    Adicionando arquivos de configuração no projeto

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+# STARSHIP
+ln config/starship.toml ~/.config/
+
+# NVIM
+ln -s $(pwd)/config/nvim ~/.config/
+
+# KITTY
+ln -s $(pwd)/config/kitty ~/.config/
+
+# FONTS
+cp fonts/*ttf ~/.local/lib/share/fonts/
